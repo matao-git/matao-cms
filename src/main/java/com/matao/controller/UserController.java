ab0b310cfd34f286979b1bad912896d7dbc841e3
matao-git <2237362443@qq.com>
2019-11-12 13:29:25 +0800

    框架运行起来并作出jsp界面

--- src/main/java/com/matao/controller/UserController.java
+++ src/main/java/com/matao/controller/UserController.java
@@ -4,10 +4,13 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+	//httpxxxx://user/hello
+	@RequestMapping(value="hello",method=RequestMethod.GET)
 	public String tet(HttpServletRequest request) {
 		request.setAttribute("info", "hello");
 		return "user/test";
