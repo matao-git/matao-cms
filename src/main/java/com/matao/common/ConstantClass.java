de43143f57abbc59c9e45db4bd9db815b062290d
matao-git <2237362443@qq.com>
2019-11-13 13:04:17 +0800

    实体类编写

---
+++ src/main/java/com/matao/common/ConstantClass.java
@@ -0,0 +1,15 @@
+package com.matao.common;
+/**
+ * 
+    * @ClassName: ConstantClass
+    * @Description: 保存CMS系统的常量
+    * @author 马涛
+    * @date 2019年11月13日
+    *
+ */
+public class ConstantClass {
+	/**
+	 * 普通用户
+	 */
+	public static final int USER_ROLE_GENERAL=0;
+}
---
+++ src/main/java/com/matao/entity/User.java
@@ -0,0 +1,183 @@
+package com.matao.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.matao.common.ConstantClass;
+
+public class User implements Serializable{
+
+	
+	    /**
+	    * @Fields serialVersionUID : TODO(用一句话描述这个变量表示什么)
+	    */
+	    
+	private static final long serialVersionUID = -7301890011337102989L;
+	
+	private Integer id;
+	
+	private String username;
+	
+	private String password;
+	
+	private String nickname;
+	
+	private Date birthday;
+	//性别
+	private int gender;
+	//是否被解禁
+	private int  locked;
+	//注册时间
+	private Date createTime;
+	private Date updateTime;
+	//没有用上
+	private String url;
+	// 没有用上
+	private int score;
+	
+	private int role = ConstantClass.USER_ROLE_GENERAL;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public int getGender() {
+		return gender;
+	}
+	public void setGender(int gender) {
+		this.gender = gender;
+	}
+	public int getLocked() {
+		return locked;
+	}
+	public void setLocked(int locked) {
+		this.locked = locked;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((birthday == null) ? 0 : birthday.hashCode());
+		result = prime * result + ((createTime == null) ? 0 : createTime.hashCode());
+		result = prime * result + gender;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + locked;
+		result = prime * result + ((nickname == null) ? 0 : nickname.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + score;
+		result = prime * result + ((updateTime == null) ? 0 : updateTime.hashCode());
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (birthday == null) {
+			if (other.birthday != null)
+				return false;
+		} else if (!birthday.equals(other.birthday))
+			return false;
+		if (createTime == null) {
+			if (other.createTime != null)
+				return false;
+		} else if (!createTime.equals(other.createTime))
+			return false;
+		if (gender != other.gender)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (locked != other.locked)
+			return false;
+		if (nickname == null) {
+			if (other.nickname != null)
+				return false;
+		} else if (!nickname.equals(other.nickname))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (score != other.score)
+			return false;
+		if (updateTime == null) {
+			if (other.updateTime != null)
+				return false;
+		} else if (!updateTime.equals(other.updateTime))
+			return false;
+		if (url == null) {
+			if (other.url != null)
+				return false;
+		} else if (!url.equals(other.url))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+	
+}
